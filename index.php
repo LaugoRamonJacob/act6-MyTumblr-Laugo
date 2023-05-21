8560be1f4bacd1375f33a17f9d2e70a12d981c4d
LaugoRamonJacob <laugojacob@gmail.com>
2023-05-21 20:32:04 +0800

    buttons function fix has been added

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ if(isset($_REQUEST['login_button']) === true){
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
 						
 
-						
+
 						<form method="POST" class="login-form">
 		      		
 		      		<div class="form-group">
@@ -95,10 +95,10 @@ if(isset($_REQUEST['login_button']) === true){
 		      			
 
 	
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name="form_username" required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name="form_password" required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -112,7 +112,7 @@ if(isset($_REQUEST['login_button']) === true){
 								</div>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="login_button">Get Started</button>
 	            </div>
 	          </form>
 	        </div>
@@ -120,12 +120,5 @@ if(isset($_REQUEST['login_button']) === true){
 			</div>
 		</div>
 	</section>
-
-  <!--<script src="js/jquery.min.js"></script>
-  <script src="js/popper.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script> -->
-
 	</body>
 </html>
-
